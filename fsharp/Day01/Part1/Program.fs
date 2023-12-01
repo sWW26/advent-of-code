7fca511c7f94eab6720af8dd9dd1a1dcaf5fec5b
Sam Woodward <sam@woodwardsoftware.co.uk>
2023-12-01 22:26:08 +0000

    day 1 part 1 fsharp

---
+++ fsharp/Day01/Part1/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+open System.IO
+
+File.ReadLines "input.txt"
+|> Seq.sumBy (fun line ->
+    let first = line |> Seq.find Char.IsDigit
+    let last = line |> Seq.findBack Char.IsDigit
+    int (first - '0') * 10 + int (last - '0'))
+|> printfn "%i"
+
